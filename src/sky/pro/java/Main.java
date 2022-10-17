b210a7678c5b5b9031066def4a5b54faea25a71f
Albert <albert_vasenin@mail.ru>
2022-10-17 17:30:58 +0400

    Second commit

--- src/sky/pro/java/Main.java
+++ src/sky/pro/java/Main.java
@@ -3,7 +3,11 @@ package sky.pro.java;
 public class Main {
 
   public static void main(String[] args) {
-
+    Author authorOne = new Author("Габриэль Гарсиа", "Маркес");
+    Book bookOne = new Book("Сто лет одиночества", authorOne, 1967);
+    Author authorTwo = new Author("Олдос", "Хаксли");
+    Book bookTwo = new Book("О дивный новый мир", authorTwo, 1932);
+    bookTwo.setPublicationYear(1948);
   }
 
 }
